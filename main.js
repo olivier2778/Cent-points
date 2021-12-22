6e8b231e5c3accfe7ce1f0f1ee879064a4d5952c
Olivier <delpouve@club-internet.fr>
2021-12-22 20:43:27 +0100

    3e commit logique jeu 2 joueurs

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 
 // voir si 2 niveau de jeu avec niveau 1 : + 100 points ou plus et niveau 2 avec le juste score de 100 points 
 
-let globalPlayer1 = 0
-let roundPlayer1 = 0
-let resultPlayer1 = 0
+let globalPlayer1
+let globalPlayer2
+let roundPlayer1
+let roundPlayer2
+let diceResult
+let playerNumberStart 
 
-let globalPlayer2 = 0
-let roundPlayer2 = 0
-let resultPlayer2 = 0
-
-let numberPlayer = 1
 const maxNumberPlayers = 2
 
 const maxScore = 100
@@ -18,37 +16,106 @@ const looseRound = 1
 
 document.getElementById("buttonPlay").onclick = play
 document.getElementById("buttonHold").onclick = hold
+document.getElementById("buttonNewGame").onclick = init
 
 
-// determine la valeur du lancer et passe le tour si resultat du lancé = 1
+// initilaise les valeurs au debut du jeu ou de la partie
 
-function play() {
+function init() {
 
-    resultPlayer1 = Math.floor(Math.random() * (6 )) + 1
+    globalPlayer1 = 0
+    roundPlayer1 = 0
 
-    document.getElementById("resultPlayer1").textContent = resultPlayer1
+    diceResult = 0    
 
-    if (resultPlayer1 !== 1) {
-        roundPlayer1 = roundPlayer1 + resultPlayer1
-        document.getElementById("roundPlayer1").textContent = roundPlayer1
-        
-    } else {
-        roundPlayer1 = 0
-        document.getElementById("roundPlayer1").textContent = 0
-    }
+    globalPlayer2 = 0
+    roundPlayer2 = 0
+
+    document.getElementById("roundPlayer1").textContent = roundPlayer1
+    document.getElementById("roundPlayer2").textContent = roundPlayer2
+
+    document.getElementById("globalPlayer1").textContent = globalPlayer1
+    document.getElementById("globalPlayer2").textContent = globalPlayer2
+
+    choosePlayer ()    
+
 }
 
 
 
-function hold() {       
+function choosePlayer() {
+
+    playerNumberStart = Math.floor(Math.random() * (2)) + 1
+   
+}
+
+
+// determine la valeur du lancer et passe le tour si resultat du lancé est 1
+
+function play() {
+
+    diceResult = Math.floor(Math.random() * (6)) + 1
+
+    document.getElementById("dice").textContent = diceResult
+
+    if ( playerNumberStart === 1) {
+
+        if (diceResult !== 1) {
+            roundPlayer1 = roundPlayer1 + diceResult
+            document.getElementById("roundPlayer1").textContent = roundPlayer1
+    
+        } else {
+            roundPlayer1 = 0
+            document.getElementById("roundPlayer1").textContent = 0
+            playerNumberStart = 2
+        }
+
+    } else {
+
+        if (diceResult !== 1) {
+            roundPlayer2 = roundPlayer2 + diceResult
+            document.getElementById("roundPlayer2").textContent = roundPlayer2
+    
+        } else {
+            roundPlayer2 = 0
+            document.getElementById("roundPlayer2").textContent = 0
+            playerNumberStart = 1
+        }
+
+    }
+    
+}
+
+
+// valide le score provisoire effectué et l'ajoute au score global et ensuite passe le tour au joueur suivant
+
+function hold() {
+
+    if ( playerNumberStart === 1) {
 
     globalPlayer1 = globalPlayer1 + roundPlayer1
     document.getElementById("globalPlayer1").textContent = globalPlayer1
 
     roundPlayer1 = 0
-    document.getElementById("roundPlayer1").textContent = 0
+    document.getElementById("roundPlayer1").textContent = 0 
+
+    playerNumberStart = 2
+
+    } else {
+
+    globalPlayer2 = globalPlayer2 + roundPlayer2
+    document.getElementById("globalPlayer2").textContent = globalPlayer2
+
+    roundPlayer2 = 0
+    document.getElementById("roundPlayer2").textContent = 0 
+
+    playerNumberStart = 1
+
+    }
 }
 
+
+init ()
 
 
 
